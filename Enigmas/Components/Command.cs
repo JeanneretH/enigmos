18c277e18de310dabb727f83c8ae3e2fb1d399f2
Jeanneret Hugo (CPLN - ET) <JeanneretH2@s2.rpn.ch>
2015-12-10 12:26:29 +0100

    Création de la classe PC.

---
+++ Enigmas/Components/Command.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cpln.Enigmos.Enigmas.Components
+{
+    class Command
+    {
+        public static Command DEFAULT = new Command("", "Erreur : commande inconnue");
+        private string strNom;
+        private string strResultat;
+
+        public Command(string nom, string resultat)
+        {
+            this.strNom = nom;
+            this.strResultat = resultat;
+        }
+
+        public string getNom()
+        {
+            return strNom;
+        }
+    }
+}
---
+++ Enigmas/Components/PC.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cpln.Enigmos.Enigmas.Components
+{
+    class PC
+    {
+        private List<Command> commands = new List<Command>();
+
+        public void AddCommand(string nom, string resultat)
+        {
+            commands.Add(new Command(nom, resultat));
+        }
+
+        public Command GetCommand(string nom)
+        {
+            foreach (Command command in commands)
+            {
+                if (nom == command.getNom())
+                {
+                    return command;
+                }
+            }
+
+            /*
+            foreach équivalent à :
+            for (int i = 0; i < commands.Count; i++)
+            {
+                Command command = commands[i];
+                // ...
+            }
+            */
+
+            return Command.DEFAULT;
+        }
+    }
+}
