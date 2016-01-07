70cdb019192f1dd6ddf0492f2cdeb3420c2b843c
Jeanneret Hugo (CPLN - ET) <JeanneretH2@s2.rpn.ch>
2016-01-07 09:27:59 +0100

    Commentaire du code

--- Enigmas/Components/Command.cs
+++ Enigmas/Components/Command.cs
@@ -12,27 +12,48 @@ namespace Cpln.Enigmos.Enigmas.Components
         private string strResultat;
         private PC destination = null;
 
+        /// <summary>
+        /// Constructeur de la classe commande
+        /// </summary>
+        /// <param name="nom">nom de la commande</param>
+        /// <param name="resultat">resultat de la commande</param>
         public Command(string nom, string resultat)
         {
             this.strNom = nom;
             this.strResultat = resultat;
         }
 
+        /// <summary>
+        /// Accède au nom de l'objet commande
+        /// </summary>
+        /// <returns>Son nom</returns>
         public string getNom()
         {
             return strNom;
         }
 
+        /// <summary>
+        /// Accède au resultat de la commande
+        /// </summary>
+        /// <returns>Son resultat</returns>
         public string getResultat()
         {
             return strResultat;
         }
 
+        /// <summary>
+        /// Défini vers quel pc la commande va pointer
+        /// </summary>
+        /// <param name="destination">le nom du pc de destination</param>
         public void setDestination(PC destination)
         {
             this.destination = destination;
         }
 
+        /// <summary>
+        /// Accède à la destination de la commande
+        /// </summary>
+        /// <returns>La destination</returns>
         public PC getDestination()
         {
             return destination;
--- Enigmas/Components/PC.cs
+++ Enigmas/Components/PC.cs
@@ -10,11 +10,21 @@ namespace Cpln.Enigmos.Enigmas.Components
     {
         private List<Command> commands = new List<Command>();
 
+        /// <summary>
+        /// Ajoute une commande à la liste
+        /// </summary>
+        /// <param name="nom">Nom de la commande</param>
+        /// <param name="resultat">Resultat de la commande</param>
         public void AddCommand(string nom, string resultat)
         {
             commands.Add(new Command(nom, resultat));
         }
 
+        /// <summary>
+        /// Vérifie que la commande existe
+        /// </summary>
+        /// <param name="nom">nom de la commande</param>
+        /// <returns>retourne la commande si elle existe, sinon un message d'erreur</returns>
         public Command GetCommand(string nom)
         {
             foreach (Command command in commands)
